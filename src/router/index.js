e0b47300e68266c979983f5e316c71be451cdaa3
Anita <anita@AnitadeMacBook-Pro.local>
2025-11-13 13:26:40 +0800

    更新main.js 和router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import OrderBookMock from '../views/OrderBookMock.vue';
-import OrderBookReal from '../views/OrderBookReal.vue';
+// import OrderBookMock from '../views/OrderBookMock.vue';
+// import OrderBookReal from '../views/OrderBookReal.vue';
+import Dashboard from '../views/Dashboard.vue';
 
 const routes = [
-  { path: '/', redirect: '/mock' },
-  { path: '/mock', component: OrderBookMock },
-  { path: '/real', component: OrderBookReal },
+  // { path: '/', redirect: '/mock' },
+  // { path: '/mock', component: OrderBookMock },
+  // { path: '/real', component: OrderBookReal },
+  { path: '/', name: 'dashboard', component: Dashboard },
 ];
 
 const router = createRouter({
